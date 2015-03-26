c7130ba8449fdbcba313a8377b4a47a2d7e60093
matt <matthew.d.duran@gmail.com>
2015-03-26 15:46:19 -0400

    added initialized files and a few other things. Prototyping the front end

---
+++ app/index.js
@@ -0,0 +1,33 @@
+var http = require('http');
+var url = require('url');
+var express = require('express');
+var bodyParser = require('body-parser');
+var faye = require('faye');
+var request = require('request');
+
+var server_url = 'http://localhost:8000/';
+
+
+//create faye adapter
+var bayeux = new faye.NodeAdapter({mount: '/comm'});
+var app = express();
+app.use(bodyParser());
+
+app.post('/game', function (req, res) {
+	console.log("request made");
+	bayeux.getClient().publish('/' + req.body.gameID, {
+  		type: 'winner',
+  		userID: parseInt(req.body.winnerID),
+	});
+	res.writeHead(202);
+  	res.end();
+});
+
+var server = app.listen(2000, function(){
+	var host = server.address().address;
+	var port = server.address().port;
+});
+
+//finalize server, attach faye and listen to port
+
+bayeux.attach(server);
